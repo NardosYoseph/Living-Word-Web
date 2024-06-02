d42d0262819cd4820a50a9aca8d724521e12d906
NardosYoseph <nardosyosef123@gmail.com>
2024-06-02 17:57:37 +0300

    living word

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="footer text-black">
       <div className="container p-4 sm:p-12 flex flex-col sm:flex-row justify-between items-center">
         {/* Set width and height for the image */}
-        <img src="images/newlogo1.jpg" alt="Cassio Logo" width="200" height="90" />
+        <img src="images/logo.jpg" alt="Cassio Logo" width="200" height="90" />
         <p className="text-slate-600">@2024 Living Word.</p>
         <p className="text-slate-600">All rights reserved.</p>
         {/* Mention the development company */}
