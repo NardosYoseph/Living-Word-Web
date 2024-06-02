9b6d1ebf0fe3d406c9057c9983655a4deb5a40ba
NardosYoseph <nardosyosef123@gmail.com>
2024-06-02 14:57:51 +0300

    living word

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <p className="text-slate-600">@2024 Living Word.</p>
         <p className="text-slate-600">All rights reserved.</p>
         {/* Mention the development company */}
-        <p className="text-slate-600">Website by 
+        <p className="text-slate-600">
         <a href="https://example.com" target="_blank" rel="noopener noreferrer" className="text-blue-500">Cassiopeia Technologies</a></p>
       </div>
     </footer>
