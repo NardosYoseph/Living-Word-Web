27e1977222f9619f2755d8719946b2cc87ead80f
NardosYoseph <nardosyosef123@gmail.com>
2024-09-05 15:43:23 +0300

    new

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,13 @@ import Head from "next/head";
 export default function Home() {
   return (
     <>
-      <Head>
+      <head>
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9635869266478553"
           crossOrigin="anonymous"
         ></script>
-      </Head>
+      </head>
       <main className="flex min-h-screen flex-col bg-white ">
         <HomePage />
       </main>
