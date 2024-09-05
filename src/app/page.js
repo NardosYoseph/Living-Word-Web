b3c1c3b617aaed165fc8d0ce94f322d86b364284
NardosYoseph <nardosyosef123@gmail.com>
2024-09-05 15:39:59 +0300

    new

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,19 @@
-
 import HomePage from "./pages/page";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-white ">
-      <HomePage />
-    </main>
+    <>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9635869266478553"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <main className="flex min-h-screen flex-col bg-white ">
+        <HomePage />
+      </main>
+    </>
   );
 }
